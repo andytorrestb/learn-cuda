69908727173697b371c75bc1b713a486facad456
andytorrestb <andytorrestb@gmail.com>
2025-08-01 23:53:48 -0400

    feature(one-week-tutorial/ch7-gpu-memory/memory.cu): added simple exercize to demonstrate differences between local thread index and the global thread index.

--- one-week-tutorial/ch7-gpu-memory/memory.cu
+++ one-week-tutorial/ch7-gpu-memory/memory.cu
@@ -1,1 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+__global__ void kernel()
+{
+    int temp = 0;
+    temp = threadIdx.x;
+
+    printf("blockIdx.x: %d, blockDim.x: %d, threadIdx.x: %d, temp: %d\n",
+           blockIdx.x, blockDim.x, threadIdx.x, temp);
+
+}
+
+__global__ void kernel2()
+{
+    int temp = 0;
+    temp = blockIdx.x * blockDim.x + threadIdx.x;
+
+    printf("blockIdx.x: %d, blockDim.x: %d, threadIdx.x: %d, temp: %d\n",
+           blockIdx.x, blockDim.x, threadIdx.x, temp);
+}
+
+int main()
+{
+    // Q: Why is this kernel function only printing threadIdx.x values from 0 to 4?
+    // When the expected output should ranging from 0 to 24?
+
+    // A: Because each block exists in its own memory space (thus resetting the threadIdx.x values for each block).
+    // How can this be fixed?
+    kernel<<<5, 5>>>();
+    cudaDeviceSynchronize();
+
+
+    printf("=================================================\n");
+
+
+    // This can be fixed by calculating the global thread index
+    // using the formula: blockIdx.x * blockDim.x + threadIdx.x
+    kernel2<<<5, 5>>>();
+    cudaDeviceSynchronize();
+
+    return 0;
+}
+
+
+// The kernel functions should be re-named to reflect their purpose (and adhere to readability conentions).
+// The first kernel can be named `printLocalThreadIdx` and the second one can be named `printGlobalThreadIdx`.
+// However, the current names are kept for demonstration purposes.
